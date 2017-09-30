e76342681b46dbaead0d0c2a69d6eadf1f54b84f
mike <esanu@ualberta.ca>
2017-09-30 13:45:10 -0600

    adding selection for fig 8 or circles

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include "USBMouse.h"
 #include "rtos.h"
 DigitalOut myled(LED1);
+DigitalIn circlesEn(D8);
+DigitalIn fig8En(D9);
 
 void goInCircles(USBMouse *mouseP){
   Circle circle(60,5);
@@ -25,6 +27,21 @@ void goInFig8(USBMouse *mouseP){
 int main() {
   USBMouse mouse;
   Thread thread;
-  thread.start(&mouse,goInFig8);
-  Thread::wait(osWaitForever);
+  myled.write(1);
+
+  circlesEn.mode(PullDown);
+  fig8En.mode(PullDown);
+  while(1){
+     if(circlesEn.read() == 1)
+     {
+          thread.start(&mouse,goInCircles);
+          Thread::wait(osWaitForever);
+     }
+     else if(fig8En.read() == 1)
+     {
+          thread.start(&mouse,goInFig8);
+          Thread::wait(osWaitForever);
+     }
+     Thread::wait(1);
+     }
 }
